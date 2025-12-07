a551b5e4204117783b44668573540f0c19ba11d4
Loserpeepeehead <raywu114@yahoo.com>
2025-12-06 21:41:09 -0800

    Implemented buttons + More Organized and Repositioning
    Buttons for Tower shop
    Create new folders to hold pawn.gd and pawn_panel.gd, etc

---
+++ checkmate,-bots!/scripts/towers/knight/knight_panel.gd
@@ -0,0 +1,29 @@
+extends Panel
+
+var tower_type: String = "knight"
+var tower_cost: int = 5
+
+
+func _ready():
+	if EventBus:
+		EventBus.gold_changed.connect(_update_affordability)
+	_update_affordability(0)
+
+
+func _on_button_pressed():
+	# Get placement_system the same way world_controller uses it
+	var world = get_tree().current_scene
+	if world:
+		var placement_system = world.get_node_or_null("PlacementSystem")
+		if placement_system and placement_system.has_method("start_placement"):
+			placement_system.start_placement(tower_type, tower_cost)
+			print("Selected ", tower_type, " for placement")
+
+
+func _update_affordability(_gold_amount: int):
+	if CurrencyManager:
+		var can_afford = CurrencyManager.get_current_gold() >= tower_cost
+		if can_afford:
+			modulate = Color(1.0, 1.0, 1.0, 1.0)
+		else:
+			modulate = Color(0.5, 0.5, 0.5, 1.0)
